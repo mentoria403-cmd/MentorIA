56177ceec0583a2d3884f7e8cc2bba7ac0e7af1b
acarlosmachado34-commits <acarlosmachado34@gmail.com>
2026-05-21 11:14:43 -0300

    adicionado dashboarb

---
+++ scriptDashboarb.js
@@ -0,0 +1,163 @@
+// PEGAR USUÁRIO
+
+const usuario =
+  JSON.parse(localStorage.getItem("usuario"));
+
+// MOSTRAR NOME
+
+if(usuario) {
+
+  document.getElementById(
+    "mensagemUsuario"
+  ).innerText =
+    `Olá, ${usuario.nome} 👋`;
+}
+
+// TAREFAS
+
+let tarefas =
+  JSON.parse(localStorage.getItem("tarefas"))
+  || [];
+
+function adicionarTarefa() {
+
+  const input =
+    document.getElementById("tarefaInput");
+
+  const texto = input.value;
+
+  if(texto === "") {
+
+    alert("Digite uma tarefa!");
+
+    return;
+  }
+
+  tarefas.push(texto);
+
+  localStorage.setItem(
+    "tarefas",
+    JSON.stringify(tarefas)
+  );
+
+  input.value = "";
+
+  renderizarTarefas();
+}
+
+function renderizarTarefas() {
+
+  const lista =
+    document.getElementById("listaTarefas");
+
+  lista.innerHTML = "";
+
+  tarefas.forEach((tarefa, index) => {
+
+    lista.innerHTML += `
+      <li>
+        ${tarefa}
+
+        <button
+          class="remover-btn"
+          onclick="removerTarefa(${index})"
+        >
+          X
+        </button>
+      </li>
+    `;
+  });
+}
+
+function removerTarefa(index) {
+
+  tarefas.splice(index, 1);
+
+  localStorage.setItem(
+    "tarefas",
+    JSON.stringify(tarefas)
+  );
+
+  renderizarTarefas();
+}
+
+// METAS
+
+let metas =
+  JSON.parse(localStorage.getItem("metas"))
+  || [];
+
+function adicionarMeta() {
+
+  const input =
+    document.getElementById("metaInput");
+
+  const texto = input.value;
+
+  if(texto === "") {
+
+    alert("Digite uma meta!");
+
+    return;
+  }
+
+  metas.push(texto);
+
+  localStorage.setItem(
+    "metas",
+    JSON.stringify(metas)
+  );
+
+  input.value = "";
+
+  renderizarMetas();
+}
+
+function renderizarMetas() {
+
+  const lista =
+    document.getElementById("listaMetas");
+
+  lista.innerHTML = "";
+
+  metas.forEach((meta, index) => {
+
+    lista.innerHTML += `
+      <li>
+        ${meta}
+
+        <button
+          class="remover-btn"
+          onclick="removerMeta(${index})"
+        >
+          X
+        </button>
+      </li>
+    `;
+  });
+}
+
+function removerMeta(index) {
+
+  metas.splice(index, 1);
+
+  localStorage.setItem(
+    "metas",
+    JSON.stringify(metas)
+  );
+
+  renderizarMetas();
+}
+
+// SAIR
+
+function sair() {
+
+  window.location.href = "login.html";
+}
+
+// INICIAR
+
+renderizarTarefas();
+
+renderizarMetas();
